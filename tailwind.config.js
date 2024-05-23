8da200c09f08429ed393cc0b479fc72ae6f7592b
Lehh <123740554+LeticiaMeloSilv@users.noreply.github.com>
2024-05-23 17:23:22 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // fontFamily: {
+    //   'OleoScript': "url('https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap');"
+    // },
+    extend: {
+      fontFamily: {
+        'OleoScript': ['Oleo Script']
+      },
+      colors:{
+        'laranja-escuro':'#FFD274',
+        'laranja-claro':'#FFDD95',
+        'azul-escuro':'#2A00A8',
+        'azul-claro':'#86A7FC',
+        'azul-titulo':'#496989'
+      },
+      backgroundImage: {
+        'logo': "url('./img/search.png')",
+        'patas': "url('./img/patas.png')",
+        'arranhado': "url('./img/arranhado.png')",
+
+      },
+    },
+  },
+  plugins: [],
+}
